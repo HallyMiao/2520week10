4bde2e85af551a53847734a9c3448bd10a7dd6e5
Hally Miao <hmiao2@my.bcit.ca>
2018-11-14 10:03:06 -0800

    getting ready to deploy

--- server.js
+++ server.js
@@ -67,6 +67,6 @@ app.get('/404', (request, response) => {
 	})
 });
 
-app.listen(8080, () => {
-	console.log('Server is up on the port 8080');
+app.listen(port, () => {
+	console.log(`Server is up on the port ${port}`);
 });
